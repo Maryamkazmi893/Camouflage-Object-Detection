405f460ddb0dc197521f6b69260d75e00b1c403d
Maryamkazmi893 <meryemshah26@gmail.com>
2025-07-14 10:39:36 +0500

    Add files via upload

---
+++ cvit-main-2/dataset.py
@@ -0,0 +1,153 @@
+import os
+import cv2
+import torch
+from torch.utils import data
+import numpy as np
+import random
+
+random.seed(10)
+
+
+class ImageDataTrain(data.Dataset):
+    def __init__(self, data_root, data_list, image_size):
+        self.sal_root = data_root
+        self.sal_source = data_list
+        self.image_size = image_size
+
+        with open(self.sal_source, 'r') as f:
+            self.sal_list = [x.strip() for x in f.readlines()]
+
+        self.sal_num = len(self.sal_list)
+
+    def __getitem__(self, item):
+        # Load image and GT mask
+        im_name = self.sal_list[item % self.sal_num].split()[0]
+        gt_name = self.sal_list[item % self.sal_num].split()[1]
+
+        sal_image, im_size = load_image(os.path.join(self.sal_root, im_name), self.image_size)
+        sal_label, sal_edge = load_sal_label(os.path.join(self.sal_root, gt_name), self.image_size)
+
+        # Data augmentation (only RGB and GT)
+        sal_image, sal_label = cv_random_crop_rgb_gt(sal_image, sal_label, self.image_size)
+
+        sal_image = sal_image.transpose((2, 0, 1))
+        sal_label = sal_label.transpose((2, 0, 1))
+        sal_edge = sal_edge.transpose((2, 0, 1))
+
+        sal_image = torch.Tensor(sal_image)
+        sal_label = torch.Tensor(sal_label)
+        sal_edge = torch.Tensor(sal_edge)
+
+        sample = {'sal_image': sal_image, 'sal_label': sal_label, 'sal_edge': sal_edge}
+        return sample
+
+    def __len__(self):
+        return self.sal_num
+
+
+class ImageDataTest(data.Dataset):
+    def __init__(self, data_root, data_list, image_size):
+        self.data_root = data_root
+        self.data_list = data_list
+        self.image_size = image_size
+
+        with open(self.data_list, 'r') as f:
+            self.image_list = [x.strip() for x in f.readlines()]
+
+        self.image_num = len(self.image_list)
+
+    def __getitem__(self, item):
+        image_path = self.image_list[item].split()[0]
+        image, im_size = load_image_test(os.path.join(self.data_root, image_path), self.image_size)
+        image = torch.Tensor(image)
+
+        return {'image': image, 'name': image_path.split('/')[-1], 'size': im_size}
+
+    def __len__(self):
+        return self.image_num
+
+
+def get_loader(config, mode='train', pin=True):
+    shuffle = (mode == 'train')
+    if mode == 'train':
+        dataset = ImageDataTrain(config.train_root, config.train_list, config.image_size)
+    else:
+        dataset = ImageDataTest(config.test_root, config.test_list, config.image_size)
+
+    data_loader = data.DataLoader(dataset=dataset,
+                                  batch_size=config.batch_size,
+                                  shuffle=shuffle,
+                                  num_workers=config.num_thread,
+                                  pin_memory=pin)
+    return data_loader
+
+
+def load_image(path, image_size):
+    if not os.path.exists(path):
+        print(f'File {path} not exists')
+    im = cv2.imread(path)
+    in_ = np.array(im, dtype=np.float32)
+    im_size = tuple(in_.shape[:2])
+    in_ = cv2.resize(in_, (image_size, image_size))
+    in_ = Normalization(in_)
+    return in_, im_size
+
+
+def load_image_test(path, image_size):
+    if not os.path.exists(path):
+        print(f'File {path} not exists')
+    im = cv2.imread(path)
+    in_ = np.array(im, dtype=np.float32)
+    im_size = tuple(in_.shape[:2])
+    in_ = cv2.resize(in_, (image_size, image_size))
+    in_ = Normalization(in_)
+    in_ = in_.transpose((2, 0, 1))
+    return in_, im_size
+
+
+def load_sal_label(path, image_size):
+    if not os.path.exists(path):
+        print(f'File {path} not exists')
+    im = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+
+    # Edge detection
+    gX = cv2.Sobel(im, ddepth=cv2.CV_32F, dx=1, dy=0, ksize=3)
+    gY = cv2.Sobel(im, ddepth=cv2.CV_32F, dx=0, dy=1, ksize=3)
+    gX = cv2.convertScaleAbs(gX)
+    gY = cv2.convertScaleAbs(gY)
+    combined = cv2.addWeighted(gX, 0.5, gY, 0.5, 0)
+    combined = np.array(combined, dtype=np.float32)
+    combined = cv2.resize(combined, (image_size, image_size))
+    combined = combined / 255.0
+    combined = combined[..., np.newaxis]
+
+    label = np.array(im, dtype=np.float32)
+    label = cv2.resize(label, (image_size, image_size))
+    label = label / 255.0
+    label = label[..., np.newaxis]
+
+    return label, combined
+
+
+def cv_random_crop_rgb_gt(image, label, image_size):
+    crop_size = int(0.0625 * image_size)
+    cropped = image_size - crop_size
+    top = random.randint(0, crop_size)
+    left = random.randint(0, crop_size)
+
+    image = image[top: top + cropped, left: left + cropped, :]
+    label = label[top: top + cropped, left: left + cropped, :]
+
+    image = cv2.resize(image, (image_size, image_size))
+    label = cv2.resize(label, (image_size, image_size))
+    label = label[..., np.newaxis]
+
+    return image, label
+
+
+def Normalization(image):
+    in_ = image[:, :, ::-1] / 255.0
+    in_ -= np.array((0.485, 0.456, 0.406))
+    in_ /= np.array((0.229, 0.224, 0.225))
+    return in_
+
